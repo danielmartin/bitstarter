99fee54ee1ee954e78115d9322e4101981e4c40d
danielmartin <mardani29@yahoo.es>
2013-07-21 18:35:54 +0000

    Initial redesign using Bootstrap. Change Node.js server to reload index file in a loop

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buf = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
+  var buf = fs.readFileSync('index.html');
   response.send(buf.toString());
 });
 
